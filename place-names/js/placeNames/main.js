10f9a4afec50019aa8ca9d7712671d4f67d6652f
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-23 15:24:06 -0400

    placenames

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,20 @@
 // Put your code here
-
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
-];
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+ ];
+ 
+ console.log("All Place Names");
+ 
+ for (const name of names) {
+   console.log(name);
+ }
 
-console.log("All Pace Names");
+ const theNames = [
+    "The back yard", "The mall", "The Great Wall of China"
+ ]
+ console.log("'The' Place Names");
 
-for (const name of names) {
-  console.log(name);
-}
+ for (const theName of theNames) {
+    console.log(theName)
+ }
