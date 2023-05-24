214195e899fff91582a4cd07a4e4fe106610787b
Marquise Campbell <marquisetyrrell@gmail.com>
2023-05-24 14:25:43 -0400

    dice with the help of Sarah and friends :)

--- enemies-list/csharp/EnemiesList/Program.cs
+++ enemies-list/csharp/EnemiesList/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
 Console.WriteLine("My Enemies List!");
--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,50 @@
 // Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
+  const dieString = (die) => {
+    if (die === 1) {
+        return "one"
+    } else if (die === 2)
+{
+    return "two"
+}  
+    else if (die === 3) {
+        return "three"
+    }
+
+    else if (die === 4) {
+        return "four"
+    }
+
+    else if (die === 5) {
+        return "five"
+    }
+    else if (die === 6) {
+        return "6ix"
+    }
+
+}
+for (let i = 0; i < 10; i++)
+{
+    let die1 = getRandomInt(1,6);
+    let die2 = getRandomInt(1,6)
+
+    let message = `${die1} + ${die2} == ${die1 + die2}`;
+    if (die1 == die2)
+    {
+        message += " DOUBLES!";
+    }
+
+    console.log(message);
+}
